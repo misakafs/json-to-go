5a74ba0cc94c47c4592fb135fca252d490be95f8
lhlyu <2438211370@qq.com>
2021-06-06 17:46:31 +0800

    add humps

--- src/core/codegen_go.ts
+++ src/core/codegen_go.ts
@@ -1,3 +1,4 @@
+import humps from 'humps/index'
 import { TokenType } from './token'
 import { Node, NodeType } from './parse'
 
@@ -27,6 +28,31 @@ export function getGoKind(tokenType: TokenType): GoKind {
         default:
             return GoKind.INTERFACE
     }
+}
+
+// Tag的命名方式
+const tagNamedWays = {
+    // abcDef
+    0: function(s:string):string {
+        return humps.camelize(s)
+    },
+    // AbcDef
+    1: function(s:string):string {
+        return humps.pascalize(s)
+    },
+    // abc_def
+    2: function(s:string):string {
+        return humps.decamelize(s)
+    },
+    // Abcdef
+    3: function(s:string):string {
+        s = humps.decamelize(s,{ separator: '' })
+        return s.replace(s[0], s[0].toUpperCase())
+    },
+    // abcdef
+    4: function(s:string):string {
+        return humps.decamelize(s,{ separator: '' })
+    },
 }
 
 class GoNode {
