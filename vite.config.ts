f29fea6c199dd6356179a8322b81084a572314df
lhlyu <2438211370@qq.com>
2021-06-15 12:03:38 +0800

    set base

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import externalGlobals from 'rollup-plugin-external-globals'
 
+const base = process.env.PROD ? '/json-to-x/' : '/'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: base,
     build: {
         rollupOptions: {
             external: ['ace-builds'],
